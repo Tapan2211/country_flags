3ff38706b61c93c5309bb2983cea64d1122af729
Tapan Ghataliya <tapanghataliya@Tapans-MacBook-Air.local>
2024-02-01 18:01:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import "./App.css";
+
+function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error("Error fetching data ", error));
+  }, []);
+
+  const onPressFlag =(countryName)=>{
+    console.log("Flag", countryName)
+  }
+
+  const cardStyle = {
+    width: "200px",
+    border: "1px solid #ccc",
+    borderRadius: "10px",
+    margin: "10px",
+    padding: "10px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+
+  const imageStyle = {
+    width: "100px",
+    height: "100px"
+  }
+
+  const containerStyle = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: "100vh"
+  }
+
+  return (
+    <div style={containerStyle}>
+      {
+        countries.map((country) => (
+          <div key={country.cca3} style={cardStyle}>
+            <img style={imageStyle} src={country.flags.png} alt={`Flag of ${country.name.common}`} />
+            <h2 className='countryName'>{country.name.common}</h2>
+          </div>
+        ))
+      }
+    </div>
+  );
+}
+
+export default App;
